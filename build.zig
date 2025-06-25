f7bb7a2e0a75b710138dccf2bf482ad3f7d0f28d
Vanya A. Sergeev <v@sergeev.io>
2025-06-25 08:48:04 -0500

    update version to v0.2.0

--- build.zig
+++ build.zig
@@ -31,7 +31,7 @@ pub fn build(b: *std.Build) !void {
 
     const build_options = b.addOptions();
     build_options.addOption([]const []const u8, "FRONTEND_ASSETS", frontend_assets.items);
-    build_options.addOption([]const u8, "VERSION", "0.1.0");
+    build_options.addOption([]const u8, "VERSION", "0.2.0");
 
     const exe = b.addExecutable(.{
         .name = "radfly",
